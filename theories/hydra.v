7b5fac873a4bcf1a41cd0552e692caa998c8fbe3
DmxLarchey <larchey@gmail.com>
2023-10-15 20:38:49 +0200

    converted to list path ordering

--- theories/hydra.v
+++ theories/hydra.v
@@ -188,85 +188,67 @@ Section list_order.
      least relation containing contextual reduction
      and closed under left permutation *)
 
-  Inductive mso_step : list X → list X → Prop :=
-    | mso_step_intro l m x r : m <ₗ x 
-                             → l++m++r ⊏ l++[x]++r
-    | mso_step_perm_l l m k  : l ~ₚ m 
-                             → l ⊏ k 
-                             → m ⊏ k
-  where "l ⊏ m" := (mso_step l m).
+  Inductive lo_step : list X → list X → Prop :=
+    | lo_step_intro l m x r : m <ₗ x → l++m++r ⊏ l++[x]++r
+  where "l ⊏ m" := (lo_step l m).
 
-  Hint Constructors mso_step : core.
+  Hint Constructors lo_step : core.
 
   (* The inversion lemma gives an alternate characterization *)
-  Fact mso_step_inv k p : 
-         k ⊏ p ↔ ∃ l m x r, k ~ₚ l++m++r ∧ p = l++[x]++r ∧ m <ₗ x.
+  Fact lo_step_inv k p : 
+         k ⊏ p ↔ ∃ l m x r, k = l++m++r ∧ p = l++[x]++r ∧ m <ₗ x.
   Proof.
     split.
-    + induction 1 as [ l m x r | ? ? ? ? ? (l' & m' & x' & r' & ? & -> & ?) ].
-      * now exists l, m, x, r.
-      * exists l', m', x', r'; eauto.
-    + intros (? & ? & ? & ? & ? & -> & ?); eauto.
+    + induction 1 as [ l m x r ].
+      now exists l, m, x, r.
+    + intros (? & ? & ? & ? & -> & -> & ?); eauto.
   Qed.
 
-  Fact mso_step_inv_sg_rt l x : l ⊏ [x] ↔ l <ₗ x.
+  Fact lo_step_inv_sg_rt l x : l ⊏ [x] ↔ l <ₗ x.
   Proof.
-    rewrite mso_step_inv; split.
-    + intros (a & m & y & b & H1 & H2 & H3).
+    rewrite lo_step_inv; split.
+    + intros (a & m & y & b & -> & H2 & H3).
       destruct a as [ | ? [] ]; try easy; simpl in *.
       inversion H2; subst y b.
-      rewrite <- app_nil_end in H1.
-      revert H3; apply perm_forall_less; auto.
+      now rewrite <- app_nil_end.
     + exists [], l, x, []; simpl; rewrite <- app_nil_end; auto.
   Qed.
 
-  Fact mso_step_inv_sg_lft l x : [x] ⊏ l → x ∈ l ∧ l ≠ [x] ∨ ∃ y, y ∈ l ∧ x < y.
+  Fact lo_step_inv_sg_lft l x : [x] ⊏ l → x ∈ l ∧ l ≠ [x] ∨ ∃ y, y ∈ l ∧ x < y.
   Proof.
-    intros (a & m & y & b & H1%Permutation_length_1_inv & H2 & H3)%mso_step_inv.
+    intros (a & m & y & b & H1 & -> & H3)%lo_step_inv.
     destruct a as [ | x' a ].
-    2:{ simpl in H1; apply cons_inj in H1 as (<- & (-> & (-> & ->)%app_nil_inv)%app_nil_inv).
+    2:{ symmetry in H1; simpl in H1. 
+        apply cons_inj in H1 as (<- & (-> & (-> & ->)%app_nil_inv)%app_nil_inv).
         left; subst; simpl; split; now auto. }
     simpl in *.
     destruct m as [ | x' m ].
-    2:{ simpl in H1; apply cons_inj in H1 as (<- & (-> & ->)%app_nil_inv). 
+    2:{ symmetry in H1; simpl in H1. 
+        apply cons_inj in H1 as (<- & (-> & ->)%app_nil_inv). 
         right; exists y; subst; split; auto. }
     simpl in *; subst; left; split; now auto.
   Qed.
 
   (* ⊏ is contextually closed *)
-  Fact mso_step_ctx l r u v : u ⊏ v → l++u++r ⊏ l++v++r.
+  Fact lo_step_ctx l r u v : u ⊏ v → l++u++r ⊏ l++v++r.
   Proof.
     induction 1 in l, r |- *; eauto.
     rewrite !app_ass, <- ! (app_ass l); eauto.
   Qed.
 
-  (* ⊏ is closed under right permutation *)
-  Fact mso_step_perm_r l m k : l ~ₚ m → k ⊏ l → k ⊏ m.
-  Proof.
-    intros H1 H2; revert H2 m H1.
-    induction 1 as [ | ? ? ? ? ? IH ]; intro.
-    + intros (? & ? & -> & ?)%Permutation_middle_inv; eauto.
-    + intros ?%IH; eauto.
-  Qed.
-
   (* The empty list is minimal for ⊏ *)
-  Fact mso_step_nil_inv l : ~ l ⊏ [].
-  Proof. now intros ([|] & ? & ? & ? & ? & ? & ?)%mso_step_inv. Qed.
+  Fact lo_step_nil_inv l : ~ l ⊏ [].
+  Proof. now intros ([|] & ? & ? & ? & ? & ? & ?)%lo_step_inv. Qed.
 
-  Fact mso_step_cons x l : l ⊏ x::l.
-  Proof. now apply @mso_step_intro with (l := []) (m := []); simpl. Qed.
+  Fact lo_step_cons x l : l ⊏ x::l.
+  Proof. now apply @lo_step_intro with (l := []) (m := []); simpl. Qed.
 
-  Notation W := (Acc mso_step).
+  Notation W := (Acc lo_step).
 
-  Hint Resolve mso_step_perm_r Acc_intro : core.
+  Fact Acc_lo_step_nil : W [].
+  Proof. constructor 1; intros _ []%lo_step_nil_inv. Qed.
 
-  Fact Permutation_Acc_mso_step l m : l ~ₚ m → W m → W l.
-  Proof. intros ? []; eauto. Qed.
-
-  Fact Acc_mso_step_nil : W [].
-  Proof. constructor 1; intros _ []%mso_step_nil_inv. Qed.
-
-  Section Acc_mso_step_cons.
+  Section Acc_lo_step_cons.
 
     Let W_app_bound y r :
         (∀x, x < y → ∀l, W l → W (x::l))
@@ -278,12 +260,12 @@ Section list_order.
       intros; apply hy; eauto.
     Qed.
 
-    Let mso_step_cons_right_inv k y m : 
+    Let lo_step_cons_right_inv k y m : 
           k ⊏ y::m 
-        → (∃ u, k ~ₚ u ++ m ∧ u <ₗ y)
-        ∨ (∃ l u x r, m = l++[x]++r ∧ k ~ₚ y::l++u++r ∧ u <ₗ x).
+        → (∃ u, k = u ++ m ∧ u <ₗ y)
+        ∨ (∃ l u x r, m = l++[x]++r ∧ k = y::l++u++r ∧ u <ₗ x).
     Proof.
-      intros ([ | z l] & u & x & r & hk & e & hu)%mso_step_inv; simpl in *;
+      intros ([ | z l] & u & x & r & hk & e & hu)%lo_step_inv; simpl in *;
       apply cons_inj in e as [-> ->]; [ left | right ]; eauto.
       exists l, u, x, r; eauto.
     Qed.
@@ -295,36 +277,33 @@ Section list_order.
          → W (y::m).
     Proof.
       intros hy hm ihm; constructor.
-      intros k [ (u & hk & hu) | (l & u & x & r & -> & hk & hu) ]%mso_step_cons_right_inv.
-      + apply Permutation_Acc_mso_step with (1 := hk).
-        apply (W_app_bound _ _ hy hm _ hu).
-      + apply Permutation_Acc_mso_step with (1 := hk), ihm; eauto.
+      intros k [ (u & -> & hu) | (l & u & x & r & -> & -> & hu) ]%lo_step_cons_right_inv; eauto.
     Qed.
 
     Let W_cons y : (∀x, x < y → ∀l, W l → W (x::l)) → ∀l, W l → W (y::l).
     Proof. induction 2; eauto. Qed.
 
-    Local Lemma Acc_mso_step_cons : ∀x, Acc R x → ∀l, W l → W (x::l).
+    Local Lemma Acc_lo_step_cons : ∀x, Acc R x → ∀l, W l → W (x::l).
     Proof. induction 1; eauto. Qed.
 
-  End Acc_mso_step_cons.
+  End Acc_lo_step_cons.
 
-  Hint Resolve Acc_mso_step_nil 
-               Acc_mso_step_cons : core.
+  Hint Resolve Acc_lo_step_nil 
+               Acc_lo_step_cons : core.
 
   (* W is closed under [] and x::_ for any accessible x
      so it contains any list composed of accessibles *) 
-  Local Lemma Forall_Acc_mso_step l : Forall (Acc R) l → W l.
+  Local Lemma Forall_Acc_lo_step l : Forall (Acc R) l → W l.
   Proof. induction 1; eauto. Qed.
 
-  Definition mso := clos_trans mso_step.
+  Definition lo := clos_trans lo_step.
 
-  Infix "⊏⁺" := mso.
+  Infix "⊏⁺" := lo.
 
-  (** Closure properties of mso/⊏⁺ *)
+  (** Closure properties of lo/⊏⁺ *)
 
   (* The constructor for the basic reduction *)
-  Fact mso_intro x l : l <ₗ x → l ⊏⁺ [x].
+  Fact lo_intro x l : l <ₗ x → l ⊏⁺ [x].
   Proof.
     constructor 1.
     rewrite (app_nil_end l).
@@ -332,100 +311,89 @@ Section list_order.
   Qed.
 
   Hint Constructors clos_trans : core.
-  Hint Resolve mso_step_ctx : core.
+  Hint Resolve lo_step_ctx : core.
 
   (* Contextual closure *)
-  Fact mso_ctx l r u v : u ⊏⁺ v → l++u++r ⊏⁺ l++v++r.
-  Proof. unfold mso; induction 1 in l, r |- *; eauto. Qed.
+  Fact lo_ctx l r u v : u ⊏⁺ v → l++u++r ⊏⁺ l++v++r.
+  Proof. unfold lo; induction 1 in l, r |- *; eauto. Qed.
 
   (* Transitivity *)
-  Fact mso_trans l m p : l ⊏⁺ m → m ⊏⁺ p → l ⊏⁺ p.
+  Fact lo_trans l m p : l ⊏⁺ m → m ⊏⁺ p → l ⊏⁺ p.
   Proof. econstructor 2; eassumption. Qed.
 
-  Hint Resolve mso_step_perm_l mso_step_perm_r : core.
-
-  (* Closure under left/right permutations *)
-
-  Fact mso_perm_l l m k : l ~ₚ m → l ⊏⁺ k → m ⊏⁺ k.
-  Proof. unfold mso; intros H1 H2; revert m H1; induction H2; eauto. Qed.
-
-  Fact mso_perm_r l m k : l ~ₚ m → k ⊏⁺ l → k ⊏⁺ m.
-  Proof. unfold mso; intros H1 H2; revert m H1; induction H2; eauto. Qed.
-
-  Fact mso_erase l x r : l++r ⊏⁺ l++[x]++r.
-  Proof. apply mso_ctx with (u := []), mso_intro; now simpl. Qed.
+  Fact lo_erase l x r : l++r ⊏⁺ l++[x]++r.
+  Proof. apply lo_ctx with (u := []), lo_intro; now simpl. Qed.
 
   (* Closure under right head/tail append *)
-  Fact mso_app_head l m r : m ⊏⁺ r → m ⊏⁺ l++r.
+  Fact lo_app_head l m r : m ⊏⁺ r → m ⊏⁺ l++r.
   Proof.
     intros H.
     induction l as [ | x l IH ]; simpl; auto. 
-    apply mso_trans with (1 := IH),
-          mso_erase with (l := []).
+    apply lo_trans with (1 := IH),
+          lo_erase with (l := []).
   Qed.
 
-  Fact mso_app_tail l m r : m ⊏⁺ l → m ⊏⁺ l++r.
+  Fact lo_app_tail l m r : m ⊏⁺ l → m ⊏⁺ l++r.
   Proof.
     intros H.
     induction r as [ | x r IH ]; simpl; auto.
     + now rewrite <- app_nil_end.
-    + apply mso_trans with (1 := IH),
-            mso_erase.
+    + apply lo_trans with (1 := IH), lo_erase.
   Qed. 
 
-  Hint Resolve mso_step_cons : core.
+  Hint Resolve lo_step_cons : core.
 
-  Fact mso_cons x l : l ⊏⁺ x::l.
+  Fact lo_cons x l : l ⊏⁺ x::l.
   Proof. red; auto. Qed.
 
-  Lemma Acc_mso_forall l : Acc mso l → ∀x, x ∈ l → Acc R x.
+  Lemma Acc_lo_forall l : Acc lo l → ∀x, x ∈ l → Acc R x.
   Proof.
     induction 1 as [ l _ IHl ]; intros x Hx.
     constructor; intros y Hy.
     apply IHl with (y := [y]); auto.
     apply in_split in Hx as (l' & r' & ->).
-    apply mso_app_head, mso_app_tail with (l := [_]).
-    apply mso_intro; now intros ? [ <- | [] ].
+    apply lo_app_head, lo_app_tail with (l := [_]).
+    apply lo_intro; now intros ? [ <- | [] ].
   Qed.
 
-  Hint Resolve Acc_mso_forall Forall_Acc_mso_step Acc_clos_trans : core.
+  Hint Resolve Acc_lo_forall Forall_Acc_lo_step Acc_clos_trans : core.
 
   (* This is the main theorem characterizing mso Acc(essibility) *)
-  Theorem Acc_mso_iff l : Acc mso l ↔ Forall (Acc R) l.
-  Proof. unfold mso; split; eauto; rewrite Forall_forall; eauto. Qed.
+  Theorem Acc_lo_iff l : Acc lo l ↔ Forall (Acc R) l.
+  Proof. unfold lo; split; eauto; rewrite Forall_forall; eauto. Qed.
 
-  Corollary wf_mso : well_founded R → well_founded mso.
-  Proof. intros ? ?; apply Acc_mso_iff, Forall_forall; auto. Qed.
+  Corollary wf_mso : well_founded R → well_founded lo.
+  Proof. intros ? ?; apply Acc_lo_iff, Forall_forall; auto. Qed.
  
-End multiset_order.
+End list_order.
 
-Arguments mso {_}.
+Arguments lo {_}.
 
-Section multiset_path_ordering.
+Section list_path_ordering.
 
-  (* Weak variant of the multiset path ordering on hydra
+  (* Weak variant of the list path ordering on hydra
      one could add two other constructors and keep it wf *)
 
   Unset Elimination Schemes.
 
-  Inductive mpo : hydra → hydra → Prop :=
-    | mpo_list l m : mso mpo l m → mpo ⟨l⟩ ⟨m⟩.
+  Inductive lpo : hydra → hydra → Prop :=
+    | lpo_intro l m : lo lpo l m → lpo ⟨l⟩ ⟨m⟩.
 
   Set Elimination Schemes.
 
-  Hint Resolve Acc_mso_forall : core.
+  Hint Resolve Acc_lo_forall : core.
 
-  Theorem wf_mpo : well_founded mpo.
+  Theorem wf_lpo : well_founded lpo.
   Proof. 
     intros h; induction h as [ l IHl ].
-    revert IHl; rewrite <- Forall_forall, <- Acc_mso_iff.
+    revert IHl; rewrite <- Forall_forall, <- Acc_lo_iff.
     induction 1 as [ m _ IHm ].
     constructor.
     intro g; induction g.
     inversion 1; eauto.
   Qed.
 
-End multiset_path_ordering.
+End list_path_ordering.
 
 #[local] Notation "⨸" := ⟨[]⟩.
 #[local] Notation only_copies x c := (∀ y, y ∈ c → x = y).
@@ -440,36 +408,36 @@ Section hercules.
     | hcut_d1 l r : hcut ⟨l⟞⨸⟝r⟩ ⟨l⟛r⟩
     | hcut_d2 h h' : hcut_deep h h' → hcut h h'.
 
-  Local Fact cut_mpo l r : mpo ⟨l⟛r⟩ ⟨l⟞⨸⟝r⟩.
+  Local Fact cut_lpo l r : lpo ⟨l⟛r⟩ ⟨l⟞⨸⟝r⟩.
   Proof.
     constructor.
     change r with ([]++r).
-    now apply mso_ctx, mso_intro.
+    now apply lo_ctx, lo_intro.
   Qed.
 
-  Hint Resolve cut_mpo : core.
+  Hint Resolve cut_lpo : core.
 
-  Local Lemma hcut_deep_mpo g h : hcut_deep h g → mpo g h.
+  Local Lemma hcut_deep_lpo g h : hcut_deep h g → lpo g h.
   Proof.
     induction 1 as [ l0 r0 l1 r1 c Hc | l h h' r H IH ].
     + constructor.
       rewrite app_ass.
-      apply mso_ctx, mso_intro.
+      apply lo_ctx, lo_intro.
       intros ? <-%Hc; auto.
     + constructor.
-      apply mso_ctx, mso_intro.
+      apply lo_ctx, lo_intro.
       now intros ? [ <- | [] ].
   Qed.
 
-  Hint Resolve hcut_deep_mpo : core.
+  Hint Resolve hcut_deep_lpo : core.
 
-  Local Lemma hcut_mpo g h : hcut h g → mpo g h.
+  Local Lemma hcut_lpo g h : hcut h g → lpo g h.
   Proof. induction 1; auto. Qed.
 
-  Hint Resolve hcut_mpo : core.
+  Hint Resolve hcut_lpo : core.
 
   Theorem hydra_terminating h : terminating hcut h.
-  Proof.  apply wf_incl with (2 := wf_mpo); red; eauto. Qed.
+  Proof.  apply wf_incl with (2 := wf_lpo); red; eauto. Qed.
 
   (* Now we study a sequence of moves *)
 
@@ -554,48 +522,3 @@ End hercules.
 Print move.
 Check hydra_steps.
 Print Assumptions hydra_steps.
-
-
-(*
-Fact fold_right_conj {X} (P : rel₁ X) l :
-   fold_right (λ x p, P x ∧ p) True l ↔ Forall P l.
-Proof.
-  induction l; simpl.
-  + split; eauto.
-  + now rewrite IHl, Forall_cons_iff.
-Qed.
-
-Section hydra_fall.
-
-  Variable P : hydra → Prop.
-
-  Fixpoint hydra_fall h : Prop :=
-    match h with
-    | ⟨l⟩ => P ⟨l⟩ ∧ fold_right (λ x p, hydra_fall x ∧ p) True l
-    end.
-
-  Fact hydra_fall_fix l :
-      hydra_fall ⟨l⟩ ↔ P ⟨l⟩ ∧ ∀h, h ∈ l → hydra_fall h.
-  Proof. now simpl; rewrite fold_right_conj, <- Forall_forall. Qed.
-
-  Section hydra_fall_rect.
-
-    Variables (Q : hydra → Type)
-              (HQ : ∀l, P ⟨l⟩
-                     → (∀h, h ∈ l → hydra_fall h)
-                     → (∀h, h ∈ l → Q h)
-                     → Q ⟨l⟩).
-
-    Theorem hydra_fall_rect h : hydra_fall h → Q h.
-    Proof.
-      induction h as [ l IH ]; intros H.
-      rewrite hydra_fall_fix in H; destruct H; auto.
-    Qed.
-
-  End hydra_fall_rect.
-
-End hydra_fall.
-*)
-
-  
-
