5b276979966ae42809096901c86cf82376a1f5ab
DmxLarchey <larchey@gmail.com>
2023-10-22 10:15:44 +0200

    cleanup in the construction of epsilon0

--- theories/epsilon0.v
+++ theories/epsilon0.v
@@ -189,95 +189,83 @@ Section llt_wf.
 
   Implicit Type (x : X).
 
-  Hint Constructors ordered_from : core.
+  Hint Constructors ordered_from clos_refl_trans : core.
 
-  Inductive nprefix x n : list X -> Prop :=
-    | nprefix_0 h : Forall (eq x) h -> length h = n -> nprefix x n h
-    | nprefix_1 y h m : Forall (eq x) h -> length h = n -> R y x -> ordered_from (ge R) y m -> nprefix x n (h++m).
+  Hint Resolve ordered_from_clos_trans : core.
 
-  Hint Constructors nprefix : core.
+  Fact clos_t_ge_rt x y : clos_trans (ge R) x y → clos_refl_trans R y x.
+  Proof. induction 1 as [ ? ? [ <- | ] | ]; eauto. Qed.
 
-  Fact nprefix_S x n l : nprefix x n l -> nprefix x (S n) (x::l).
-  Proof.
-    intros [ h H1 H2 | y h m H1 H2 H3 ].
-    + constructor 1; simpl; eauto.
-    + constructor 2 with (y := y) (h := _::_); simpl; eauto.
-  Qed.
+  Hint Resolve clos_t_ge_rt clos_rt_t ordered_cons_inv : core.
 
-  Hint Resolve nprefix_S : core.
-
-  Fact ordered_from_nprefix x l : ordered_from (ge R) x l -> exists n, nprefix x n l.
-  Proof.
-    induction 1 as [ | x y l [ <- | H1 ] H2 (n & Hn) ].
-    + exists 0; constructor 1; auto.
-    + exists (S n).
-      destruct Hn as [ l H3 H4 | y h m H3 H4 H5 H6 ].
-      * constructor 1; simpl; auto.
-      * constructor 2 with (h := _::_) (y := y); simpl; auto.
-    + exists 0.
-      destruct Hn as [ l H3 H4 | z h m H3 H4 H5 H6 ];
-        constructor 2 with (y := y) (h := []); simpl; eauto.
-  Qed.
-
-  Fact ordered_from_ge_inv x l : ordered_from (ge R) x l -> Forall (eq x) l \/ exists y h m, R y x /\ l = h++m /\ Forall (eq x) h /\ ordered_from (ge R) y m.
-  Proof.
-    induction 1 as [ | x y l [ <- | H1 ] H2 IH2 ]; eauto.
-    + destruct IH2 as [ ? | (y & h & m & ? & -> & ? & ?) ]; auto; right.
-      exists y, (x::h), m; simpl; auto.
-    + right; destruct IH2 as [ ? | (z & h & m & ? & -> & ? & ?) ].
-      * exists y, [], (y::l); simpl; repeat split;eauto.
-      * exists y, [], (y::h++m); repeat split; auto.
-  Qed.
-
-  Hint Resolve ordered_from_ordered : core.
-
-  (* False : Counter-example ltt [3;1] [3;2] *)  
-  Fact llt_nprefix_inv l m x n : 
-              llt R l m -> nprefix x n m -> ordered (ge R) l -> (exists p, p < n /\ nprefix x p l) \/ exists y k, R y x /\ nprefix y k l.
+  Fact ordered_llt_lo l m : ordered (ge R) l → llt R l m → lo (clos_trans R) l m.
   Proof.
     intros H1 H2; revert H2 H1.
-    intros [ h H1 H2 | y h m' H1 H2 H3 ].
-    + clear m. 
-      intros H3; revert H3 x n H1 H2.
-      induction 1 as [ y h | x y l m H1 | x l m H1 IH1 ]; intros z n G1 G2 G3.
-      * left; exists 0; subst; simpl; split; auto; lia.
-      * right; exists x.
-        apply ordered_inv, ordered_from_nprefix in G3 as (k & Hk).
-        apply Forall_cons_iff in G1 as [ -> G1 ].
-        exists (S k); split; auto.
-      * apply Forall_cons_iff in G1 as [ -> G1 ].
-        apply ordered_inv in G3.
-        destruct (IH1 _ _ G1 eq_refl)
-          as [ (p & G4 & G5) | (y & k & G4 & G5) ]; eauto.
-        - left; exists (S p); split; eauto.
-          subst; simpl; lia.
-  Admitted.
-
-  Lemma llt_Acc_prefix x n l : Acc R x -> nprefix x n l -> Acc (λ l m, ordered (ge R) l ∧ llt R l m) l.
-  Proof.
-    intros H; revert H n l.
-    induction 1 as [ x Hx IHx ].
-    induction n as [n IHn] using (well_founded_induction lt_wf).
-    intros m Hm; constructor; intros l (Hl & H).
-    destruct (llt_nprefix_inv H Hm Hl) as [ (p & ? & ?) | (y & k & ? & ?) ]; eauto.
+    induction 1 as [ x m | x y l m H1 | x l m H1 IH1 ]; intros H.
+    + apply lo_app_tail with (l := [_]), lo_intro; now simpl.
+    + apply lo_app_tail with (l := [_]), lo_intro.
+      apply ordered_inv in H.
+      intros ? [ <- | ]; eauto.
+    + apply lo_cons; eauto.
   Qed.
 
-  Hint Resolve llt_Acc_prefix : core.
 
   (* This is the lemma we want to show but the proof is not going to work that way *)
   Lemma llt_Acc_ordered_from x l : Acc R x → ordered_from (ge R) x l → Acc (λ l m, ordered (ge R) l ∧ llt R l m) l.
-  Proof. intros ? (? & ?)%ordered_from_nprefix; eauto. Qed.
-
-  Hint Resolve llt_Acc_ordered_from : core.
-
-  Theorem llt_Acc P l :
-        Forall P l
-     -> Forall (Acc (λ x y, P x ∧ R x y)) l 
-     -> ordered (ge R) l 
-     -> Acc (λ l m, Forall P l ∧ ordered (ge R) l ∧ llt R l m) l.
   Proof. Admitted.
 
 End llt_wf.
+
+Section mono.
+
+  Hint Constructors ordered_from ordered : core.
+
+  Fact ordered_from_mono X (R T : X → X → Prop) a l : 
+          (∀ x y, x ∈ a::l → y ∈ a::l → R x y → T x y)
+        → ordered_from R a l → ordered_from T a l.
+  Proof.
+    intros H1 H2; revert H2 H1.
+    induction 1; intro; eauto.
+    constructor 2; eauto.
+  Qed.
+
+  Hint Resolve ordered_from_mono : core.
+
+  Fact ordered_mono X (R T : X → X → Prop) l : 
+          (∀ x y, x ∈ l → y ∈ l → R x y → T x y)
+        → ordered R l → ordered T l.
+  Proof. induction 2; eauto. Qed.
+
+  Hint Constructors llt : core.
+  Hint Resolve in_or_app in_eq in_cons : core.
+
+  Fact llt_mono X (R T : X → X → Prop) l m : 
+          (∀ x y, x ∈ l → y ∈ m → R x y → T x y)
+        → llt R l m → llt T l m.
+  Proof.
+    intros H.
+    induction 1 as [ | | x l m H1 IH1 ]; eauto.
+    constructor 3; eauto.
+  Qed.
+
+  Fact clos_trans_mono X (R T : X → X → Prop) : 
+          (∀ x y, R x y → T x y)
+        → (∀ l m, clos_trans R l m → clos_trans T l m).
+  Proof. induction 2; eauto. Qed.
+
+  Fact lo_step_mono X (R T : X → X → Prop) :
+          (∀ x y, R x y → T x y)
+        → (∀ l m, lo_step R l m → lo_step T l m).
+  Proof. induction 2; constructor; eauto. Qed.
+
+  Hint Resolve lo_step_mono : core.
+
+  Fact lo_mono X (R T : X → X → Prop) :
+          (∀ x y, R x y → T x y)
+        → (∀ l m, lo R l m → lo T l m).
+  Proof. intro; apply clos_trans_mono; eauto. Qed.
+
+End mono.
 
 Section epsilon0.
 
@@ -305,29 +293,26 @@ Section epsilon0.
     now apply (IH _ G1).
   Qed.
 
+  Hint Resolve llt_trans : core.
+
   Theorem olt_trans f g h : olt f g → olt g h → olt f h.
   Proof.
-    revert g h.
-    induction f as [ l IHl ].
-    intros [m] [k] H1%olt_inv H2%olt_inv.
-    constructor; try tauto.
-    revert H1 H2; apply llt_trans; eauto.
+    revert g h; induction f.
+    intros [] [] ?%olt_inv ?%olt_inv; eauto.
   Qed.
 
   Hint Resolve olt_trans olt_irrefl : core.
 
   Fact olt_dec g h : { olt g h } + { ~ olt g h }.
   Proof.
-    destruct (olt_sdec g h) as [ g h H | h | g h H ].
-    + now left.
-    + now right.
-    + right; intro G; eapply olt_irrefl; eauto.
+    destruct (olt_sdec g h); eauto.
+    right; intro; eapply olt_irrefl; eauto.
   Qed.
 
   Corollary olt_trans' g h : clos_trans olt g h → olt g h.
   Proof. induction 1; eauto. Qed.
 
-  Definition oge := λ g h, g = h ∨ olt h g.
+  Definition oge := ge olt.
 
   Fact oge_dec g h : { oge g h } + { ~ oge g h }.
   Proof.
@@ -345,29 +330,9 @@ Section epsilon0.
   Fact E0_fix l : E0 ⟨l⟩ ↔ ordered oge l ∧ ∀x, x ∈ l → E0 x.
   Proof. apply hydra_fall_fix. Qed.
 
-  Fact E0_rect (P : hydra → Type) :
-          (∀l, ordered oge l 
-             → (∀x, x ∈ l → E0 x)
-             → (∀x, x ∈ l → P x)
-             → P ⟨l⟩)
-        → ∀h, E0 h → P h.
-  Proof. apply hydra_fall_rect. Qed.
-
-  Lemma olt_E0_wf h : E0 h → Acc (λ s t, E0 s ∧ olt s t) h.
-  Proof.
-    induction 1 as [ l H1 H2 IH2 ] using E0_rect.
-    assert (Acc (λ l m, Forall E0 l /\ ordered oge l ∧ llt olt l m) l).
-    + apply llt_Acc; auto; now apply Forall_forall.
-    + clear H2 IH2; revert H H1.
-      induction 1 as [ m Hm IHm ].
-      constructor.
-      intros [l] ((H2 & H3)%E0_fix & H4%olt_inv).
-      apply IHm; auto.
-      rewrite Forall_forall; auto.
-  Qed.
-
   Hint Resolve oge_dec : core.
 
+  (* E0 is a strongly decidable predicate *)
   Theorem E0_dec h : { E0 h } + { ~ E0 h }.
   Proof.
     induction h as [ l IH ].
@@ -380,60 +345,106 @@ Section epsilon0.
     + intros ? []%IH; auto.
     + right; intros []%E0_fix; apply H3; eauto.
     + left; apply E0_fix; auto.
-  Qed. 
-
-  Hint Resolve ordered_cons_inv : core.
-  Hint Constructors clos_refl_trans : core.
-
-  Lemma E0_olt_lpo g h : E0 g → E0 h → olt g h → lpo g h.
-  Proof.
-    intros H1 H2; revert g H1 h H2.
-    induction 1 as [ l Hg1 Hg2 IHg ] using E0_rect.
-    induction 1 as [ m Hh1 Hh2 _ ] using E0_rect.
-    intros H%olt_inv.
-    induction H as [ y m | x y l m | x l m H1 IH1 ].
-    + constructor.
-      apply lo_app_tail with (l := [_]).
-      now apply lo_intro.
-    + constructor.
-      apply lo_app_tail with (l := [_]).
-      apply lo_intro; auto.
-      intros z [ <- | Hz ].
-      1: apply IHg; eauto.
-      apply ordered_inv in Hg1.
-      apply IHg; eauto.
-      apply olt_trans'.
-      apply clos_rt_t with x; eauto.
-      generalize (ordered_from_clos_trans Hg1 _ Hz).
-      clear Hg2 IHg y m Hh1 Hh2 H l Hg1 Hz.
-      induction 1 as [ x z [ <- | ] | ]; eauto.
-    + constructor.
-      apply lo_cons, lpo_inv.
-      apply IH1; eauto.
   Qed.
 
-  (* Since olt is maximal on E0 ... *)
-  Corollary E0_lpo_olt g h : E0 g → E0 h → lpo g h → olt g h.
+  (* We convert E0 into an equivalent proof irrelevant predicate *)
+  Definition eps0 h := if E0_dec h then True else False.
+
+  Fact eps0_iff h : eps0 h ↔ E0 h.
+  Proof. unfold eps0; destruct (E0_dec h); tauto. Qed.
+
+  Fact eps0_pirr h (E1 E2 : eps0 h) : E1 = E2.
+  Proof.
+    unfold eps0 in *.
+    destruct (E0_dec h); [ | easy ].
+    now destruct E1; destruct E2.
+  Qed.
+
+  (* Now we work with eps0 instead of E0 *)
+
+  (* We convert the fixpoint equation *)
+  Fact eps0_fix l : eps0 ⟨l⟩ ↔ ordered oge l ∧ ∀x, x ∈ l → eps0 x.
+  Proof.
+    rewrite eps0_iff, E0_fix.
+    split; intros []; split; auto; intros; apply eps0_iff; auto.
+  Qed.
+
+  (* We convert the recursor *)
+  Fact eps0_rect (P : hydra → Type) :
+          (∀l, ordered oge l 
+             → (∀x, x ∈ l → eps0 x)
+             → (∀x, x ∈ l → P x)
+             → P ⟨l⟩)
+        → ∀h, eps0 h → P h.
+  Proof. 
+    intros HP h H%eps0_iff; revert h H.
+    induction 1 as [ l H1 H2 IH ]using hydra_fall_rect.
+    apply HP; auto.
+    intros; apply eps0_iff, H2; auto.
+  Qed.
+
+(*
+  Lemma olt_E0_wf h : E0 h → Acc (λ s t, E0 s ∧ olt s t) h.
+  Proof.
+    induction 1 as [ l H1 H2 IH2 ] using E0_rect.
+    assert (Acc (λ l m, Forall E0 l /\ ordered oge l ∧ llt olt l m) l).
+    + apply llt_Acc; auto; now apply Forall_forall.
+    + clear H2 IH2; revert H H1.
+      induction 1 as [ m Hm IHm ].
+      constructor.
+      intros [l] ((H2 & H3)%E0_fix & H4%olt_inv).
+      apply IHm; auto.
+      rewrite Forall_forall; auto.
+  Qed.
+*)
+
+
+
+  Hint Resolve ordered_cons_inv lpo_trans : core.
+  Hint Constructors clos_refl_trans : core.
+
+  Local Fact lpo_trans' l m : clos_trans lpo l m → lpo l m.
+  Proof. induction 1; eauto. Qed.
+
+  Hint Resolve llt_mono : core.
+
+  Lemma eps0_olt_lpo g h : eps0 g → eps0 h → olt g h → lpo g h.
+  Proof.
+    intros H1 H2; revert g H1 h H2.
+    induction 1 as [ l Hg1 Hg2 IHg ] using eps0_rect.
+    induction 1 as [ m Hh1 Hh2 _ ] using eps0_rect.
+    intros H%olt_inv; constructor.
+    apply lo_mono with (1 := lpo_trans').
+    apply ordered_llt_lo; eauto.
+    apply ordered_mono with (2 := Hg1).
+    intros ? ? ? ? []; eauto.
+  Qed.
+
+  (* Since olt is maximal on eps0 ... *)
+  Corollary eps0_lpo_olt g h : eps0 g → eps0 h → lpo g h → olt g h.
   Proof.
     intros Hg Hh H.
     destruct (olt_sdec g h) as [ g h G | g | g h G ]; auto.
     + contradict H; apply lpo_irrefl.
-    + apply E0_olt_lpo in G; auto.
-      destruct (@lpo_irrefl h).
-      eapply lpo_trans; eauto.
+    + apply eps0_olt_lpo in G; auto.
+      destruct (@lpo_irrefl h); eauto.
   Qed.
 
-  Hint Resolve E0_olt_lpo E0_lpo_olt : core.
+  Hint Resolve eps0_olt_lpo eps0_lpo_olt : core.
 
-  (** lpo and olt are IDENTICAL on E0 *)
-  Theorem E0_olt_lpo_iff g h : E0 g → E0 h → olt g h ↔ lpo g h.
+  (** lpo and olt are IDENTICAL on eps0 *)
+  Theorem E0_olt_lpo_iff g h : eps0 g → eps0 h → olt g h ↔ lpo g h.
   Proof. split; auto. Qed.
 
   Definition power h := ⟨[h]⟩.
 
-  Lemma olt_sons h : E0 h → match h with ⟨l⟩ => ∀g, g ∈ l → olt g h end. 
+  Lemma olt_sons h :
+             eps0 h 
+           → match h with 
+             | ⟨l⟩ => ∀g, g ∈ l → olt g h
+             end. 
   Proof.
-    induction 1 as [ m H1 H2 IH ] using E0_rect.
+    induction 1 as [ m H1 H2 IH ] using eps0_rect.
     intros [l] Hl.
     constructor.
     specialize (IH _ Hl); simpl in IH.
@@ -450,36 +461,30 @@ Section epsilon0.
           apply olt_trans with (2 := H1); eauto.
   Qed.
 
-  Fact E0_power h : E0 h → E0 (power h).
+  Fact eps0_power h : eps0 h → eps0 (power h).
   Proof.
-    intros; apply E0_fix; split.
+    intros; apply eps0_fix; split.
     + repeat constructor.
     + now intros ? [ <- | [] ].
   Qed.
 
-  Hint Resolve E0_power : core.
+  Hint Resolve eps0_power : core.
  
-  Fact olt_power h : E0 h → olt h (power h).
+  Fact olt_power h : eps0 h → olt h (power h).
   Proof. intro; apply (olt_sons (power h)); auto. Qed.
-
-  Definition eps0 h := if E0_dec h then True else False.
-
-  Fact eps0_iff h : eps0 h ↔ E0 h.
-  Proof.
-    unfold eps0.
-    destruct (E0_dec h); tauto.
-  Qed.
-
-  Fact eps0_pirr h (E1 E2 : eps0 h) : E1 = E2.
-  Proof.
-    unfold eps0 in *.
-    destruct (E0_dec h); [ | easy ].
-    now destruct E1; destruct E2.
-  Qed.
 
   Definition epsilon0 := sig eps0.
 
-  Implicit Type (o : epsilon0). 
+  Implicit Type (o : epsilon0).
+
+  Hint Resolve eps0_pirr : core.
+
+  Fact epsilon0_eq_iff o o' : o = o' ↔ proj1_sig o = proj1_sig o'.
+  Proof.
+    split.
+    + now intros <-.
+    + revert o o'; intros [] [] ?; simpl in *; subst; f_equal; auto.
+  Qed.
 
   Definition lt_epsilon0 o o' := olt (proj1_sig o) (proj1_sig o').
 
@@ -507,7 +512,7 @@ Section epsilon0.
     cut (well_founded R).
     + apply wf_incl.
       intros [] []; unfold lt0, R; simpl.
-      apply E0_olt_lpo; apply eps0_iff; auto.
+      apply eps0_olt_lpo; auto.
     + unfold R; apply wf_inverse_image.
       exact wf_lpo.
    Qed.
